bb6a7280441a553edc3113d39dfe849329ef5d8e
Jorge Contreras <jorgecontrerasostos@gmail.com>
2026-06-24 20:50:30 -0600

    chore(definitions.py): add jobs, sensors and schedules to defs

--- src/mlb_data/definitions.py
+++ src/mlb_data/definitions.py
@@ -8,6 +8,11 @@ from .assets.game_feed import game_feed
 from .assets.player_stats import player_stats
 from .assets.dbt import dbt_models
 
+from .jobs.bronze_partitioned import bronze_partitioned_job, bronze_partitioned_schedule
+from .jobs.bronze_snapshot import bronze_snapshot_schedule, bronze_snapshot_job
+
+from .sensors.dbt import on_bronze_partitioned_success, dbt_job
+
 from .resources.duckdb import duckdb_io_manager
 
 
@@ -17,4 +22,7 @@ defs = dg.Definitions(
         "io_manager": duckdb_io_manager,
         "dbt": DbtCliResource(project_dir="dbt/"),
     },
+    jobs=[bronze_partitioned_job, bronze_snapshot_job, dbt_job],
+    schedules=[bronze_partitioned_schedule, bronze_snapshot_schedule],
+    sensors=[on_bronze_partitioned_success]
 )
