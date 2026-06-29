b4c7e3f64f181d7c148a99099dc317f258fd086e
Jorge Contreras <jorgecontrerasostos@gmail.com>
2026-06-28 21:53:43 -0600

    chore: add standings table headings

--- ui/dugout.py
+++ ui/dugout.py
@@ -1,15 +1,16 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header
+from textual.widgets import Footer, Header, DataTable
 
 
 class Dugout(App):
-    """A Textual app to manage stopwatches."""
+    """Dugout is a TUI (Terminal User Interface) that displays MLB Data."""
 
     BINDINGS = [("d", "toggle_dark", "Toggle dark mode")]
 
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
         yield Header()
+        yield DataTable(id="standings_table")
         yield Footer()
 
     def action_toggle_dark(self) -> None:
@@ -18,6 +19,13 @@ class Dugout(App):
             "textual-dark" if self.theme == "textual-light" else "textual-light"
         )
 
+    def on_mount(self):
+        standings_table = self.get_widget_by_id(
+            "standings_table",
+            DataTable
+        )
+        cols = ["league", "division", "team", "wins", "losses", "pct", "games back"]
+        standings_table.add_columns(*cols)
 
 if __name__ == "__main__":
     app = Dugout()
